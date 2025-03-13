95ac8631c46058d8c28af07f4c3ada685e0a5adc
shanmathi.babu <myshanmathi864@gmail.com>
2025-03-13 10:25:42 +0530

    Session tracking and Filters

---
+++ SessionAndFilters/src/main/java/com/test/Myfilter.java
@@ -0,0 +1,43 @@
+package com.test;
+
+import jakarta.servlet.Filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import java.io.IOException;
+
+
+@WebFilter("/Myfilter")
+public class Myfilter extends HttpFilter implements Filter {
+
+	public void destroy() {
+
+		System.out.println("destroy()");
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+		System.out.println("before servlent execute");
+		
+		chain.doFilter(request, response);
+		
+		System.out.println("after servlet execute");
+		
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
