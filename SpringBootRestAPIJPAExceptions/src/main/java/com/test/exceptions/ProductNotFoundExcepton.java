629d2d3bce0e4e271186983703c9b0a94f98beaa
shanmathi.babu <myshanmathi864@gmail.com>
2025-03-18 09:07:58 +0530

    SpringBootRestAPIJPA Exception

---
+++ SpringBootRestAPIJPAExceptions/src/main/java/com/test/exceptions/ProductNotFoundExcepton.java
@@ -0,0 +1,11 @@
+package com.test.exceptions;
+
+
+public class ProductNotFoundExcepton extends RuntimeException{
+	
+	public ProductNotFoundExcepton(String msg)
+	{
+		super(msg);
+	}
+	
+}
--- SpringBootRestApiJPA/src/main/java/com/test/service/CustomerService.java
+++ SpringBootRestApiJPA/src/main/java/com/test/service/CustomerService.java
@@ -2,13 +2,45 @@ package com.test.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.test.dao.CustomerDao;
 import com.test.entity.Customer;
 
+
 public interface CustomerService {
-public Customer createCustomer(Customer cst);
-public List<Customer> updateCustomers(Customer cst);
-public List<Customer> readAllCustomers(Customer cst);
-public List<Customer> deleteCustomers(int id);
+
+	@Autowired
+	private CustomerDao dao;
+	
+	
+	public void setDao(CustomerDao dao) {
+		this.dao = dao;
+	}
+
+	public Customer createCustomers(Product p) {
+		
+		
+		
+		return dao.createCustomers(p);
+	}
+	
+	public List<Customer> readAll(){
+		
+		return dao.readAllProd();
+	}
+	
+	public List<Customer> update(Customer p){
+		dao.updateCustomers(p);
+		
+		return dao.readAllCustomers();
+	}
+	
+	public List<Customer> deleteCustomers(int id){
+		
+		dao.deleteCustomers(id);
+		return dao.readAllCustomers();
+	}
 
 
 }
